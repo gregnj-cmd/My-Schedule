360496b903b4d2ab95f47eeadc6222f26ed7098b
gregnj-cmd <gregnj-cmd@users.noreply.github.com>
2026-05-31 16:33:38 +0000

    deploy: bb24c965e8adc8a1b6b5c6804eb49b524a5424dd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3101696481" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1669060617" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
